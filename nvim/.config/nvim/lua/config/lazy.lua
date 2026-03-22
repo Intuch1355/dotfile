4cf6f527b076faf4932e4a1b715d65b7a4668af8
nathan <nathan@nathans-MacBook-Pro.local>
2026-03-22 17:20:53 +0700

    add nvim config with catppuccin color and lualine

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,3 +1,4 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -14,40 +15,84 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+
+    -- 🎨 Catppuccin
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      priority = 1000,
+      config = function()
+        require("catppuccin").setup({
+          flavour = "mocha",
+          transparent_background = true,
+          integrations = {
+            lualine = false,
+          },
+        })
+
+        vim.cmd.colorscheme("catppuccin") -- ✅ FIXED
+      end,
     },
-  },
+
+    -- 📊 Lualine
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "catppuccin/nvim" },
+      config = function()
+        local C = require("catppuccin.palettes").get_palette()
+        local transparent_bg = "NONE"
+
+        local custom_theme = {
+          normal = {
+            a = { bg = C.blue, fg = C.mantle, gui = "bold" },
+            b = { bg = C.surface0, fg = C.blue },
+            c = { bg = transparent_bg, fg = C.text },
+          },
+          insert = {
+            a = { bg = C.green, fg = C.base, gui = "bold" },
+            b = { bg = C.surface0, fg = C.green },
+          },
+          terminal = {
+            a = { bg = C.green, fg = C.base, gui = "bold" },
+            b = { bg = C.surface0, fg = C.green },
+          },
+          command = {
+            a = { bg = C.peach, fg = C.base, gui = "bold" },
+            b = { bg = C.surface0, fg = C.peach },
+          },
+          visual = {
+            a = { bg = C.mauve, fg = C.base, gui = "bold" },
+            b = { bg = C.surface0, fg = C.mauve },
+          },
+          replace = {
+            a = { bg = C.red, fg = C.base, gui = "bold" },
+            b = { bg = C.surface0, fg = C.red },
+          },
+          inactive = {
+            a = { bg = transparent_bg, fg = C.blue },
+            b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
+            c = { bg = transparent_bg, fg = C.overlay0 },
+          },
+        }
+
+        require("lualine").setup({
+          options = {
+            theme = custom_theme,
+            section_separators = "",
+            component_separators = "",
+          },
+        })
+      end,
+    },
+
+  }, -- ✅ correctly closes spec
+
+  install = { colorscheme = { "catppuccin" } }, -- also fixed name
+  checker = { enabled = true },
 })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,38 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
+
+-- Indentation and tabs
+opt.softtabstop = 4
+opt.tabstop     = 4
+opt.shiftwidth  = 4
+opt.expandtab   = true
+opt.autoindent  = true
+opt.smartindent = true
+
+-- Line numbers
+opt.number         = true
+opt.relativenumber = true
+
+-- Appearance
+opt.signcolumn   = "yes"
+opt.cursorline   = true
+opt.colorcolumn  = "80"
+opt.laststatus   = 2
+opt.showmatch    = true
+
+-- No background (transparent terminal)
+opt.termguicolors = false 
+
+-- Treat dash-separated words as a single word (dw/diw/ciw)
+opt.iskeyword:append("-")
+
+-- Keep cursor 8 lines from top/bottom
+opt.scrolloff = 8
+
+-- Backspace behavior
+opt.backspace = { "indent", "eol", "start" }
+
+-- Cursor responsiveness
+opt.updatetime = 50
+
+-- Syntax highlighting
+vim.cmd("syntax on")
